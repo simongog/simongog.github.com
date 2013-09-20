b37e2799367a0cf7a33ff86d567bf18fa930fbdb
Simon Gog <simon.gog@gmail.com>
2013-09-20 18:56:08 +1000

    Added tutorial slides

---
+++ assets/data/sdsl-slides/slide_config.js
@@ -0,0 +1,37 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Succinct Data Structure Library',
+    subtitle: 'Tutorial',
+    //eventTitle: 'Google I/O 2013',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Simon Gog',
+    company: 'The University of Melbourne',
+//    gplus: 'http://plus.google.com/1234567890',
+//    twitter: '@yourhandle',
+    www: 'http://people.eng.unimelb.edu.au/sgog/',
+    github: 'http://github.com/simongog'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
